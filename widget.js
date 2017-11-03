89317ed00373b60a94634cb8c3b48127f1c75821
John Lauer as ChiliPeppr on Github <jlauer@chilipeppr.com>
2017-11-03 01:15:11 +0000

    Made some changes to ChiliPeppr widget using Cloud9

--- widget.js
+++ widget.js
@@ -323,7 +323,7 @@ cpdefine("inline:com-chilipeppr-widget-grbl", ["chilipeppr_ready", "jquerycookie
                 $('#grbl-config-div').append(
                     '<div class="input-group" ' + 
                     'style="width:400px;margin-bottom:2px;"><div class="input-group-addon" style="width:40px;padding:0px 6px;">' + 
-                    '$' + 
+                    "$" + 
                     index_num + 
                     '</div><input class="form-control" style="height:20px;padding:0px 6px;width:100px;" id="com-chilipeppr-widget-grbl-config-' + 
                     index_num + 
